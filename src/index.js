2d49dd69129d6bcb41be838291fc0b8d5815b061
Cactuss26 <179231437+Cactuss26@users.noreply.github.com>
2026-04-10 17:59:03 +0530

    Changes Add Image Add title and style it Add Heading Phrase and style it

--- src/index.js
+++ src/index.js
@@ -1,2 +1,17 @@
 import "./styles.css"
+import backgroundimg from "./pexels-kunal-lakhotia-781256899-29173121.jpg"
 
+function init() {
+    const content = document.querySelector(".content");
+    const title = document.createElement("h1");
+    title.classList.add("title");
+    title.textContent = "The Lantern Restaurant"
+    content.appendChild(title);
+
+    const headingPhrase = document.createElement("p");
+    headingPhrase.classList.add("headingPhrase");
+    headingPhrase.textContent = "The perfect place to be when you desire a cozy dinner.";
+    content.appendChild(headingPhrase);
+}
+
+init();
--- webpack.config.js
+++ webpack.config.js
@@ -26,6 +26,18 @@ export default {
                 test: /\.css$/i,
                 use: ["style-loader", "css-loader"],
             },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: "asset/resource",
+            },
+            {
+                test: /\.html$/i,
+                use: ["html-loader"],
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: "asset/resource",
+            },
         ],
     },
 };
